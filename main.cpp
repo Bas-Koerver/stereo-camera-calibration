82cade97537e55f083906f8064d6385abc288bb4
Bas Körver <git@bas.xn--krver-jua.it>
2025-10-27 18:41:02 +0100

    Basic ChArUco board generation done and video gen.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
-#include <opencv2/highgui.hpp>
-#include <opencv2/calib3d.hpp>
 #include <opencv2/core.hpp>
-#include <opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/objdetect/charuco_detector.hpp>
 
-
-static inline void createBoard()
+static void generateVideo(const cv::Mat& image, cv::Size size)
 {
-    int squaresX = 5;
-    int squaresY = 7;
-    float squareLength = 0.04f; // in meters
-    float markerLength = 0.02f; // in meters
+    double fps = 60.0;
+    int codec = cv::VideoWriter::fourcc('M','J','P','G');
+
+    cv::Mat imageBgr;
+    cv::cvtColor(image, imageBgr, cv::COLOR_GRAY2BGR);
+
+    cv::Mat blankImage = cv::Mat::zeros(size, CV_8UC4);
+    cv::VideoWriter writer("board.avi", cv::CAP_OPENCV_MJPEG, codec, fps, size, true);
+
+    if (!writer.isOpened())
+    {
+        std::cout  << "Could not open the output video for write: " << std::endl;
+        return;
+    }
+
+    for (int i = 0; i < fps * (10 >> 1); i++)
+    {
+        writer.write(imageBgr);
+        writer.write(blankImage);
+    }
+    writer.release();
+}
+
+static void createBoard()
+{
+    int squaresX = 7;
+    int squaresY = 5;
+    int squareLength = 100; // in pixels
+    int markerLength = 70; // in pixels
     int margins = squareLength - markerLength;
 
     int borderBits = 1;
@@ -27,8 +50,10 @@ static inline void createBoard()
     board.generateImage(imageSize, boardImage, margins, borderBits);
 
     cv::imwrite("board.png", boardImage);
+    generateVideo(boardImage, imageSize);
 }
 
-int main() {
+int main()
+{
     createBoard();
 }
